62f8285797856d880f0cb66d53dc3577149987cb
UmeshTawade <tawadeu200@gmail.com>
2024-01-12 16:41:04 +0530

    Parameterization excel updated

--- tests/Test24ParameterisationUsingCSV.spec.js
+++ tests/Test24ParameterisationUsingCSV.spec.js
@@ -4,7 +4,9 @@ import { parse } from 'csv-parse/sync';
 import fs from 'fs';
 import path from 'path';
 
-const credrecords = parse (fs.readFileSync (path.join(__dirname, 'loginDetail.csv'))
+//const credrecords = parse (fs.readFileSync (path.join(__dirname, 'loginDetail.csv'))
+
+const credrecords = parse (fs.readFileSync (path.join(__dirname, 'loginDetail.csv')),
     {
         columns: true,
         skip_empty_lines: true
@@ -14,14 +16,18 @@ const credrecords = parse (fs.readFileSync (path.join(__dirname, 'loginDetail.cs
 for (const cred of credrecords) {
     test(`testing with ${cred.login}`, async ({ page }) => {
 
-        await page.goto("https://adactinhotelapp.com")          //await only used with fun
+        await page.goto("https://adactinhotelapp.com");          //await only used with fun
         //await expect(page).toHaveTitle("Adactin.com - Hotel Reservation System");
 
         await page.locator('id=username').type(cred.login); //tagname.attributeName=attributeValue
         await page.locator('id=password').fill(cred.password);
         await page.locator('id=login').click();
 
-        await expect(page).toHaveTitle('Adactin.com - Search Hotel');
+        //await expect(page).toHaveTitle('Adactin.com - Search Hotel');
+    
+        const loc=  await page.locator('id=username_show');
+        await expect(loc).toHaveAttribute('value', `Hello ${cred.login}!`);
+    
     }
     )
 }
